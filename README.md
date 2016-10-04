56f256685423bfdb4a606fda23b3e6172584fa3c
sanfrancesco <jonathan.otto+sanfrancesco@gmail.com>
2016-10-03 22:07:51 -0700

    First

---
+++ README.md
@@ -0,0 +1,24 @@
+# ReactJS hello worlds in different versions
+
+for testing [https://www.prerender.cloud](https://www.prerender.cloud)'s ability to:
+1. prerender slow ajax requests
+2. tolerate multiple react roots
+3. tolerate dangerouslySetInnerHTML
+4. set checksums considering all of the above, in multiple versions of react without user intervention
+
+## Documentation
+
+* 13 - https://github.com/facebook/react/tree/0.13-stable/docs/docs
+* 14 - https://github.com/facebook/react/tree/0.14-stable/docs/docs
+* 15 - https://github.com/facebook/react/tree/15-stable/docs/docs
+
+## Inspiration
+
+* https://gist.github.com/danawoodman/9cfddb1a0c934a35f31a
+* https://github.com/facebook/react-devtools/tree/master/test/example
+
+
+
+## CDN
+
+https://cdnjs.com/libraries/react
---
+++ scripts.js
@@ -0,0 +1,33 @@
+window.App = React.createClass({
+  getInitialState() {
+    return {
+      ajax: '',
+      numbers: Array.apply(null, Array(5)).map((n,i) => i)
+    }
+  },
+
+  doAjaxReq() {
+    window.fetch('//slowajax.herokuapp.com')
+        .then(res => res.text())
+        .then(body => this.setState(state => ({ ajax: state.ajax + body })));
+  },
+
+  componentDidMount() {
+    setTimeout(this.doAjaxReq, 500);
+    setTimeout(this.doAjaxReq, 1);
+  },
+
+  render() {
+    let els = this.state.numbers.map(n => (<span key={n}>{n}</span>));
+    return (
+      <div>
+        <h1>ReactJS test for http://www.prerender.cloud</h1>
+        <p>with 2 React roots, 2 ajax requests and dangerouslySetInnerHTML</p>
+        <div>{this.state.ajax}</div>
+        <div dangerouslySetInnerHTML={{__html: `'<&>'`}} />
+        {els}
+      </div>
+    )
+  }
+});
+
